f7954737ac917abb6125c3fba45335fe9355d39a
alex.paclin <alex.paclin@gmail.com>
2020-03-31 15:48:42 +0500

    события и классы

--- src/app/test-counter/test-counter.component.spec.ts
+++ src/app/test-counter/test-counter.component.spec.ts
@@ -31,4 +31,22 @@ describe("TestCounterComponent", () => {
 
 		expect(el.textContent).toContain(num.toString());
 	});
+
+	it("should add a green class if values of counter is even", () => {
+		component.counter = 6;
+
+		fixture.detectChanges();
+
+		let de = fixture.debugElement.query(By.css(".counter"));
+		let el: HTMLElement = de.nativeElement;
+
+		expect(el.classList.contains("green")).toBeTruthy();
+	});
+
+	it("should increment counter when increment button was clicked", () => {
+		let btn = fixture.debugElement.query(By.css("#increment"));
+		btn.triggerEventHandler("click", null);
+
+		expect(component.counter).toBe(1);
+	});
 });
