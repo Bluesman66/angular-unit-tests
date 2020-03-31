318058979fa56f41d87faac188a95f0214ccab81
alex.paclin <alex.paclin@gmail.com>
2020-03-31 11:34:18 +0500

    настройка для интеграционных тестов

--- src/app/test-counter/test-counter.component.spec.ts
+++ src/app/test-counter/test-counter.component.spec.ts
@@ -1,1 +1,20 @@
-describe("", () => {});
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestCounterComponent } from "./test-counter.component";
+
+describe("CounterComponent", () => {
+	let component: TestCounterComponent;
+	let fixture: ComponentFixture<TestCounterComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [TestCounterComponent]
+		});
+
+		fixture = TestBed.createComponent(TestCounterComponent);
+		component = fixture.componentInstance;
+	});
+
+	it("component should be created", () => {
+		expect(component).toBeDefined();
+	});
+});
